dbf4a7a441872e380a272702d2a1fb4f17378386
Ivy Patton <lilaivy@gmail.com>
2017-04-13 10:21:16 -0700

    did not even closed to finished. need more time.

---
+++ test/quiz.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const setTimeout = require('../lib/quiz-code');
+
+const start = newDate();
+const ellapsed = newDate() - start;
+
+
+describe(takeFive(), => {
+
+    it('will call the callback after 5 seconds', done => {
+     //test that the callback runs after a 5 second time out
+}).timeout(7000);
+done();
+
+});
+
